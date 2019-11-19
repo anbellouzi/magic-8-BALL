5dec7a3da82b8d3c8076cc21fa85fd87e6880386
Anas Bellouzi <anas.bellouzi@students.makeschool.com>
2019-11-19 11:48:32 -0800

    Initial Commit

---
+++ Magic8Bll/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Magic8Bll
+//
+//  Created by Anas on 11/19/19.
+//  Copyright © 2019 Anas. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
